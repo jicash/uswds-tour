35d3b2aaa28e56d5601c137325f7e1a192e7fd5f
Jidro <james.baker@noaa.gov>
2025-08-11 12:32:21 -0400

    Enables seperate library build and dev mode in vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,34 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  root: 'demo',
-  build: {
-    lib: {
-      entry: '../src/uswds-tour.js',
-      name: 'USWDSTour',
-      formats: ['es', 'iife'],
-      fileName: (format) => format === 'iife' ? 'uswds-tour.min.js' : 'uswds-tour.js'
-    },
-    outDir: '../dist',
-    emptyOutDir: true,
-    minify: 'terser',
-    rollupOptions: {
-      external: [],
-      output: {
-        globals: {},
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    // Dev server: serve demo as an app
+    return {
+      root: 'demo',
+      server: {
+        open: true
       }
-    }
-  },
-  server: {
-    open: true
+    };
+  } else {
+    // Build: output library
+    return {
+      build: {
+        lib: {
+          entry: 'src/uswds-tour.js',
+          name: 'USWDSTour',
+          formats: ['es', 'iife'],
+          fileName: (format) => format === 'iife' ? 'uswds-tour.min.js' : 'uswds-tour.js'
+        },
+        outDir: 'dist',
+        emptyOutDir: true,
+        minify: 'terser',
+        rollupOptions: {
+          external: [],
+          output: {
+            globals: {}
+          }
+        }
+      }
+    };
   }
 });
